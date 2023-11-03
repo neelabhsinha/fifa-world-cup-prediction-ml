1eccd6b31832b327b7bffa466b96e06f046fc2fb
Crazybugmaniac <apoorvasinha183@gmail.com>
2023-11-03 10:18:11 -0400

    Dataset generation from raw data

---
+++ const.py
@@ -0,0 +1,10 @@
+# Constants for data.py
+feature_candidates = ["rank_home","rank_away", "del_rank",'home_goals_mean','home_goals_mean_ln','home_goals_suf_mean','home_goals_suf_mean_ln', 'home_rank_mean',
+       'home_rank_mean_ln', 'home_points', 'home_points_ln',
+       'home_game_points_mean', 'home_game_points_mean_ln', 
+       'away_goals_mean', 'away_goals_mean_ln', 'away_goals_suf_mean',
+       'away_goals_suf_mean_ln', 'away_rank_mean', 'away_rank_mean_ln',
+       'away_points', 'away_points_ln', 'away_game_points_mean',
+       'away_game_points_mean_ln', 'goals_AvB_past', 'goals_BvA_past',
+       'A_victories', 'B_victories','Team_ties']
+target_list = ["Target_Outcome_Win","Target_Outcome_Loss","Target_Outcome_Tie"]
--- data_loader/data.py
+++ data_loader/data.py
@@ -7,7 +7,7 @@ import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from sklearn.feature_selection import RFE
-
+from const import *
 import warnings
 warnings.filterwarnings("ignore")
 #   DataSetSelection is meant for your use .It works on generated csvs
@@ -47,6 +47,7 @@ class DataSetSelection:  #Use this for your classifiers!
         """
         # I have verified this data with explicit hand tracing with two random football teams over a period of time for various teams.
         # however in case I missed please let me know ASAP. If you find data which is not historically true ,that means the data is wrong in the master dataset as well. 
+        #print(feature_candidates) 
         fifa_rank = self.fifarankdataset  # This is unavoidable. We need to know the ranks of the two teams on the date
         fifa_rank["rank_date"] = pd.to_datetime(fifa_rank["rank_date"]) #Fifa rank 
         fifa_rank["country_full"] = fifa_rank["country_full"].str.replace("IR Iran", "Iran").str.replace("Korea Republic", "South Korea").str.replace("USA", "United States")
@@ -271,5 +272,127 @@ class DataSetSelection:  #Use this for your classifiers!
 class DataSetGeneration:  # This generated the original Data. Use this if you need to perform hyperparameter tuning.
     def __init__(self) -> None:
         pass
-    # TODO: Dump Jupyter Code here.    
+    # TODO: Dump Jupyter Code here. 
+    def generate_unary_team_histoy(self,team_stats,n_historic_window,n_games):
+        stats_val = []
+        for index, row in tqdm(team_stats.iterrows()):
+            team = row["team"]
+            date = row["date"]
+            past_games = team_stats.loc[(team_stats["team"] == team) & (team_stats["date"] < date) & (team_stats['date'] > date- n_historic_window*pd.Timedelta(days=365) )].sort_values(by=['date'], ascending=False)
+            lastn = past_games.head(n_games)
+            
+            goals = past_games["score"].mean()
+            goals_ln = lastn["score"].mean()
+            
+            goals_suf = past_games["suf_score"].mean()
+            goals_suf_ln = lastn["suf_score"].mean()
+            
+            rank = past_games["rank_suf"].mean()
+            rank_ln = lastn["rank_suf"].mean()
+            
+            if len(lastn) > 0:
+                points = past_games["total_points"].values[0] - past_games["total_points"].values[-1]
+                points_ln = lastn["total_points"].values[0] - lastn["total_points"].values[-1] 
+            else:
+                points = 0
+                points_ln = 0
+                
+            gp = past_games["total_points"].mean()
+            gp_ln = lastn["total_points"].mean()
+            
+
+            
+            stats_val.append([goals, goals_ln, goals_suf, goals_suf_ln, rank, rank_ln,points,points_ln,gp,gp_ln])
+        stats_cols = ["goals_mean", "goals_mean_ln", "goals_suf_mean", "goals_suf_mean_ln", "rank_mean", "rank_mean_ln","points","points_ln",  "game_points_mean", "game_points_mean_ln"]
+
+        stats_df = pd.DataFrame(stats_val, columns=stats_cols)
+        return stats_df
+    def generate_masterdataset(self):
+        # Datasets
+        #1870s football data
+        historic_matches = pd.read_csv('data_loader/kaggle/input/results.csv')
+        #45100 matches
+        #Fifa Rankinf Data
+        fifa_rank = pd.read_csv('data_loader/kaggle/input/fifa_ranking-2023-07-20.csv')
+        # Let's drop all games before 1990 (fifa rank data starts here.You have 28k matches now)
+        historic_matches["date"] = pd.to_datetime(historic_matches["date"]) #Fixing non-sense dates
+        fifa_rank["rank_date"] = pd.to_datetime(fifa_rank["rank_date"]) #Fixing non-sense dates
+        historic_matches = historic_matches[(historic_matches["date"] >= "1990-1-1")].reset_index(drop=True)
+        #Let's start pasting this data
+        #Change Names
+        fifa_rank["country_full"] = fifa_rank["country_full"].str.replace("IR Iran", "Iran").str.replace("Korea Republic", "South Korea").str.replace("USA", "United States")
+        # We first add two kinds of columns one for home_team and one for away_team 
+        fifa_rank = fifa_rank.set_index(['rank_date']).groupby(['country_full'], group_keys=False).resample('D').first().fillna(method='ffill').reset_index()
+        historic_matches_ranked = historic_matches.merge(fifa_rank[["country_full", "total_points", "previous_points", "rank", "rank_change", "rank_date"]], left_on=["date", "home_team"], right_on=["rank_date", "country_full"]).drop(["rank_date", "country_full"], axis=1)
+        historic_matches_ranked = historic_matches_ranked.merge(fifa_rank[["country_full", "total_points", "previous_points", "rank", "rank_change", "rank_date"]], left_on=["date", "away_team"], right_on=["rank_date", "country_full"], suffixes=("_home", "_away")).drop(["rank_date", "country_full"], axis=1)
+        #28619 matches
+        #Tournament coded as friendly/non_friendly ( 0 and 1)
+        # This is an interesting feature. I am not convinced it should be used.
+        historic_matches_ranked["tournament"] = historic_matches_ranked.apply(lambda row : 0 if (row.tournament == "Friendly") else 1 ,axis=1)
+        # One-hot coded label generation
+        historic_matches_ranked['GoalDifference'] = historic_matches_ranked.apply(lambda row: row.home_score - row.away_score,axis=1)
+        historic_matches_ranked['Target_Outcome_Win'] = historic_matches_ranked.apply(lambda row : 1 if (row.GoalDifference > 0) else 0 ,axis=1)
+        historic_matches_ranked['Target_Outcome_Loss'] = historic_matches_ranked.apply(lambda row : 1 if (row.GoalDifference < 0) else 0 ,axis=1)
+        historic_matches_ranked['Target_Outcome_Tie'] = historic_matches_ranked.apply(lambda row : 1 if (row.GoalDifference == 0) else 0 ,axis=1)
+        #Team wise stats calculator
+        #Add new features derived from historical match data
+        # Goals taken and scored against in the last n matches (?)
+        # Unary features
+        A_team =historic_matches_ranked[["date", "home_team","home_score","away_score","rank_home", "rank_away","rank_change_home", "total_points_home", "Target_Outcome_Win","Target_Outcome_Loss","Target_Outcome_Tie"]]
+
+        B_team = historic_matches_ranked[["date", "away_team","home_score","away_score",  "rank_away", "rank_home","rank_change_away", "total_points_away", "Target_Outcome_Win","Target_Outcome_Loss","Target_Outcome_Tie"]]
+        A_team.columns = [h.replace("home_", "").replace("_home", "").replace("away_", "suf_").replace("_away", "_suf") for h in A_team.columns]
+        B_team.columns = [a.replace("away_", "").replace("_away", "").replace("home_", "suf_").replace("_home", "_suf") for a in B_team.columns]
+        team_stats = pd.concat([A_team,B_team],ignore_index=True)
+        stats_df = self.generate_unary_team_histoy(team_stats,5,5)
+        full_df = pd.concat([team_stats.reset_index(drop=True), stats_df], axis=1, ignore_index=False)
+        A_team_stats = full_df.iloc[:int(full_df.shape[0]/2),:]
+        B_team_stats = full_df.iloc[int(full_df.shape[0]/2):,:]
+        A_team_stats = A_team_stats[A_team_stats.columns[-10:]]
+        B_team_stats = B_team_stats[B_team_stats.columns[-10:]]
+        A_team_stats.columns = ['home_'+str(col) for col in A_team_stats.columns]
+        B_team_stats.columns = ['away_'+str(col) for col in B_team_stats.columns]
+        match_stats = pd.concat([A_team_stats, B_team_stats.reset_index(drop=True)], axis=1, ignore_index=False)
+        detailed_historical_matches_ranked = pd.concat([historic_matches_ranked, match_stats.reset_index(drop=True)], axis=1, ignore_index=False)
+        stats_val = []
+        n_historic_window_f2f = 5 #5 years
+        #n_games = 5 #Number of games to consider
+        for index, row in tqdm(detailed_historical_matches_ranked.iterrows()):
+            teamA = row["home_team"]
+            teamB = row["away_team"]
+            date = row["date"]
+            past_games_AvB = detailed_historical_matches_ranked[(detailed_historical_matches_ranked['home_team'] == teamA)&((detailed_historical_matches_ranked['away_team'] == teamB)) & (detailed_historical_matches_ranked["date"] < date) & (detailed_historical_matches_ranked['date'] > date- n_historic_window_f2f*pd.Timedelta(days=365) )].sort_values(by=['date'], ascending=False)
+            #lastn = past_games.head(n_games)
+            n_AvB = past_games_AvB.shape[0]
+            goals_by_A = 0
+            goals_by_B = 0
+            won_by_A =0 
+            won_by_B = 0
+            tied_AvB = 0
+            goals_by_A = past_games_AvB["home_score"].sum() + goals_by_A
+            goals_by_B = past_games_AvB["away_score"].sum() +goals_by_B
+            won_by_A = past_games_AvB["Target_Outcome_Win"].sum() +won_by_A
+            won_by_B = past_games_AvB["Target_Outcome_Loss"].sum() + won_by_B
+            tied_AvB = past_games_AvB["Target_Outcome_Tie"].sum() +tied_AvB
+            past_games_BvA =detailed_historical_matches_ranked[(detailed_historical_matches_ranked['home_team'] == teamB)&((detailed_historical_matches_ranked['away_team'] == teamA)) & (detailed_historical_matches_ranked["date"] < date) & (detailed_historical_matches_ranked['date'] > date- n_historic_window_f2f*pd.Timedelta(days=365) )].sort_values(by=['date'], ascending=False)
+            #lastn = past_games.head(n_games)
+            n_BvA = past_games_BvA.shape[0]
+            goals_by_A = past_games_BvA["away_score"].sum() + goals_by_A
+            goals_by_B = past_games_BvA["home_score"].sum() +goals_by_B
+            won_by_A = past_games_BvA["Target_Outcome_Loss"].sum() +won_by_A
+            won_by_B = past_games_BvA["Target_Outcome_Win"].sum() + won_by_B
+            tied_AvB = past_games_BvA["Target_Outcome_Tie"].sum() +tied_AvB
+            if (n_AvB + n_BvA)!=0:
+                goals_by_A = goals_by_A/(n_AvB+n_BvA)
+                goals_by_B = goals_by_B/(n_AvB+n_BvA)
+            stats_val.append([goals_by_A,goals_by_B,won_by_A,won_by_B,tied_AvB])
+        stats_cols = ["goals_AvB_past","goals_BvA_past","A_victories","B_victories","Team_ties"]
+        stats_df = pd.DataFrame(stats_val, columns=stats_cols)
+        detailed_historical_matches_ranked = pd.concat([detailed_historical_matches_ranked.reset_index(drop=True), stats_df], axis=1, ignore_index=False)
+        detailed_historical_matches_ranked = detailed_historical_matches_ranked[detailed_historical_matches_ranked["date"]>="2000-01-01"] #Final Filter
+        detailed_historical_matches_ranked_clean = detailed_historical_matches_ranked.fillna(0)
+        detailed_historical_matches_ranked_clean = detailed_historical_matches_ranked_clean.reset_index(drop=True)
+        detailed_historical_matches_ranked_clean.to_csv('data_loader/master_dataset_automated.csv',index=False)
+        return 0
+        
         
