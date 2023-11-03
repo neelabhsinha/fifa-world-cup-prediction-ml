ae2460f25a329838c13d0843f3609e591b5d98a0
Crazybugmaniac <apoorvasinha183@gmail.com>
2023-11-03 10:17:46 -0400

    Master data generation from raw datsets.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from data_loader.data import *
 data = DataSetSelection()
+new_data=DataSetGeneration()
 output = data.features_binary("Canada","Cuba",date = '2022-11-11')
-
+p = new_data.generate_masterdataset() #This generated a new file .
