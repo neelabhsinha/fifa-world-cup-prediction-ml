4ee0ef30fb2105423e3b0396ed5db6a684e53917
neelabhsinha <neelabhsinha97@gmail.com>
2023-11-06 20:22:27 -0500

    enhancement: modified features

--- const.py
+++ const.py
@@ -5,17 +5,15 @@ data_dir_path = '/Users/neelabh/Desktop/Work/Projects/CS 7641 ML/data'
 source_dir = 'source'
 
 features_keys = [
-    'home_win_count_home', 'home_win_count_away', 'away_win_count_home', 'away_win_count_away',
-    'home_team_mean_goals_scored_at_home', 'home_team_mean_goals_conceded_at_home', 'home_team_std_goals_scored_at_home',
-    'home_team_std_goals_conceded_at_home', 'home_team_mean_goals_scored_away', 'home_team_mean_goals_conceded_away',
-    'home_team_std_goals_scored_away', 'home_team_std_goals_conceded_away', 'away_team_mean_goals_scored_at_home',
-    'away_team_mean_goals_conceded_at_home', 'away_team_std_goals_scored_at_home', 'away_team_std_goals_conceded_at_home',
-    'away_team_mean_goals_scored_away', 'away_team_mean_goals_conceded_away', 'away_team_std_goals_scored_away',
-    'away_team_std_goals_conceded_away', 'h2h_home_team_mean_goals_scored_at_home', 'h2h_away_team_mean_goals_scored_away',
-    'h2h_home_team_std_goals_scored_at_home', 'h2h_away_team_std_goals_scored_away', 'h2h_home_team_mean_goals_scored_away',
-    'h2h_away_team_mean_goals_scored_at_home', 'h2h_home_team_std_goals_scored_away', 'h2h_away_team_std_goals_scored_at_home',
-    'home_team_rank', 'home_team_rank_change', 'away_team_rank', 'away_team_rank_change', 'home_match_for_home_team'
+    "home_win_count", "away_win_count", "h2h_home_win_count", "h2h_away_win_count",
+    "home_mean_goals_scored", "home_mean_goals_conceded", "home_std_goals_scored", "home_std_goals_conceded",
+    "away_mean_goals_scored", "away_mean_goals_conceded", "away_std_goals_scored", "away_std_goals_conceded",
+    "h2h_home_mean_goals", "h2h_away_mean_goals", "h2h_home_std_goals", "h2h_team_std_goals",
+    "home_rank - away_rank", "home_rank_change", "away_rank_change",
+    "home_team_opposition_rank_diff", "away_team_opposition_rank_diff",
+    "is_friendly", "is_qualifier", "is_tournament", "home_match_for_home_team"
 ]
+
 
 label_keys = ['home_team_win', 'draw', 'away_team_win']
 
--- data_loader/feature_generator.py
+++ data_loader/feature_generator.py
@@ -13,6 +13,8 @@ class FeatureGenerator:
         self._rankings_df = pd.read_csv(rankings_file)
         self._match_results_df['date'] = pd.to_datetime(self._match_results_df['date'])
         self._rankings_df['rank_date'] = pd.to_datetime(self._rankings_df['rank_date'])
+        self._match_results_df.sort_values(by=['date'], inplace=True)
+        self._rankings_df.sort_values(by=['rank_date'], inplace=True)
         self._individual_window_size = individual_window_size
         self._head_to_head_window_size = head_to_head_window_size
 
@@ -22,50 +24,41 @@ class FeatureGenerator:
         return features_labels_df
 
     def _process_row(self, row):
-        home_team, away_team, match_date = row['home_team'], row['away_team'], row['date']
+        home_team, away_team, match_date, match_type = row['home_team'], row['away_team'], row['date'], row[
+            'tournament']
         home_goals, away_goals, neutral = row['home_score'], row['away_score'], row['neutral']
-        features = self.get_features(home_team, away_team, match_date, neutral)
+        features = self.get_features(home_team, away_team, match_date, match_type, neutral)
         labels = self.get_labels(home_goals, away_goals)
         return pd.Series(np.concatenate((features.flatten(), labels.flatten())))
 
-    def get_features(self, home_team, away_team, match_date, neutral=False):
-        home_mean_goals_scored_home, home_mean_goals_conceded_home, home_std_goals_scored_home, home_std_goals_conceded_home = self._get_individual_statistics(
-            home_team, match_date, True)
-        home_mean_goals_scored_away, home_mean_goals_conceded_away, home_std_goals_scored_away, home_std_goals_conceded_away = self._get_individual_statistics(
-            home_team, match_date,False)
-        away_mean_goals_scored_home, away_mean_goals_conceded_home, away_std_goals_scored_home, away_std_goals_conceded_home = self._get_individual_statistics(
-            away_team, match_date, True)
-        away_mean_goals_scored_away, away_mean_goals_conceded_away, away_std_goals_scored_away, away_std_goals_conceded_away = self._get_individual_statistics(
-            away_team, match_date, False)
-        h2h_home_team_mean_goals_home, h2h_away_team_mean_goals_away, h2h_home_team_std_goals_home, h2h_away_std_goals_away = self._get_head_to_head_statistics(
-            home_team, away_team, match_date)
-        h2h_home_team_mean_goals_away, h2h_away_team_mean_goals_home, h2h_home_team_std_goals_away, h2h_away_std_goals_home = self._get_head_to_head_statistics(
-            away_team, home_team, match_date)
+    def get_features(self, home_team, away_team, match_date, match_type, neutral=False):
+        home_mean_goals_scored, home_mean_goals_conceded, home_std_goals_scored, home_std_goals_conceded = (
+            self._get_individual_statistics(home_team, match_date))
+        away_mean_goals_scored, away_mean_goals_conceded, away_std_goals_scored, away_std_goals_conceded = (
+            self._get_individual_statistics(away_team, match_date))
+        h2h_home_mean_goals, h2h_away_mean_goals, h2h_home_std_goals, h2h_team_std_goals = (
+            self._get_head_to_head_statistics(home_team, away_team, match_date))
         home_rank, home_rank_change = self._get_closest_ranking_for_team(home_team, match_date)
         away_rank, away_rank_change = self._get_closest_ranking_for_team(away_team, match_date)
-        home_win_count_home = self._get_home_win_count(home_team, match_date)
-        home_win_count_away = self._get_away_win_count(home_team, match_date)
-        away_win_count_home = self._get_home_win_count(away_team, match_date)
-        away_win_count_away = self._get_away_win_count(away_team, match_date)
+        home_win_count = self._get_team_win_count(home_team, match_date)
+        away_win_count = self._get_team_win_count(away_team, match_date)
         h2h_home_win_count, h2h_away_win_count = self._get_head_to_head_win_count(home_team, away_team, match_date)
+        home_team_opposition_rank_diff = self._get_average_opposition_rank_diff(home_team, match_date)
+        away_team_opposition_rank_diff = self._get_average_opposition_rank_diff(away_team, match_date)
+        is_friendly, is_qualifier, is_tournament = self._get_match_type(match_type)
         if neutral:
             home_match_for_home_team = 0
         else:
             home_match_for_home_team = 1
-        features_list = [home_win_count_home, home_win_count_away, away_win_count_home, away_win_count_away,
-                         home_mean_goals_scored_home, home_mean_goals_conceded_home, home_std_goals_scored_home,
-                         home_std_goals_conceded_home,
-                         home_mean_goals_scored_away, home_mean_goals_conceded_away, home_std_goals_scored_away,
-                         home_std_goals_conceded_away,
-                         away_mean_goals_scored_home, away_mean_goals_conceded_home, away_std_goals_scored_home,
-                         away_std_goals_conceded_home,
-                         away_mean_goals_scored_away, away_mean_goals_conceded_away, away_std_goals_scored_away,
-                         away_std_goals_conceded_away,
-                         h2h_home_team_mean_goals_home, h2h_away_team_mean_goals_away, h2h_home_team_std_goals_home,
-                         h2h_away_std_goals_away,
-                         h2h_home_team_mean_goals_away, h2h_away_team_mean_goals_home, h2h_home_team_std_goals_away,
-                         h2h_away_std_goals_home,
-                         home_rank, home_rank_change, away_rank, away_rank_change, home_match_for_home_team]
+        features_list = [
+            home_win_count, away_win_count, h2h_home_win_count, h2h_away_win_count,
+            home_mean_goals_scored, home_mean_goals_conceded, home_std_goals_scored, home_std_goals_conceded,
+            away_mean_goals_scored, away_mean_goals_conceded, away_std_goals_scored, away_std_goals_conceded,
+            h2h_home_mean_goals, h2h_away_mean_goals, h2h_home_std_goals, h2h_team_std_goals,
+            home_rank - away_rank, home_rank_change, away_rank_change,
+            home_team_opposition_rank_diff, away_team_opposition_rank_diff,
+            is_friendly, is_qualifier, is_tournament, home_match_for_home_team
+        ]
         features = np.array(features_list)[:, np.newaxis].T
         features = np.nan_to_num(features)
         return features
@@ -80,71 +73,99 @@ class FeatureGenerator:
             label_list = 0, 0, 1
         return np.array(label_list)[:, np.newaxis].T
 
-    def _get_individual_statistics(self, team, date_of_new_match, home_team):
-        self._match_results_df['date'] = pd.to_datetime(self._match_results_df['date'])
+    def _get_individual_statistics(self, team, date_of_new_match):
         matches_before_date = self._match_results_df[self._match_results_df['date'] < pd.to_datetime(date_of_new_match)]
-        home_team = 'home_team' if home_team else 'away_team'
-        home_score = 'home_score' if home_team else 'away_score'
-        away_score = 'away_score' if home_team else 'home_score'
-        team_a_matches = matches_before_date[(matches_before_date[home_team] == team)].tail(self._individual_window_size)
+        team_a_matches = matches_before_date[
+            ((matches_before_date['home_team'] == team) | (matches_before_date['away_team'] == team))].tail(
+            self._individual_window_size)
         if team_a_matches.empty:
             return 0, 0, 0, 0
-        goals_scored = team_a_matches[home_score]
-        goals_conceded = team_a_matches[away_score]
-        mean_goals_scored = goals_scored.mean()
-        mean_goals_conceded = goals_conceded.mean()
-        std_goals_scored = goals_scored.std()
-        std_goals_conceded = goals_conceded.std()
+        goals_scored = np.concatenate((team_a_matches[team_a_matches['home_team'] == team]['home_score'].to_numpy(),
+                                       team_a_matches[team_a_matches['away_team'] == team]['away_score'].to_numpy()))
+        goals_conceded = np.concatenate((team_a_matches[team_a_matches['home_team'] == team]['away_score'].to_numpy(),
+                                         team_a_matches[team_a_matches['away_team'] == team]['home_score'].to_numpy()))
+        mean_goals_scored = np.mean(goals_scored)
+        mean_goals_conceded = np.mean(goals_conceded)
+        std_goals_scored = np.std(goals_scored)
+        std_goals_conceded = np.std(goals_conceded)
         return mean_goals_scored, mean_goals_conceded, std_goals_scored, std_goals_conceded
+
+    def _get_average_opposition_rank_diff(self, team, date_of_new_match):
+        matches_before_date = self._match_results_df[self._match_results_df['date'] < pd.to_datetime(date_of_new_match)]
+        team_a_matches = matches_before_date[
+            ((matches_before_date['home_team'] == team) | (matches_before_date['away_team'] == team))].tail(
+            self._individual_window_size)
+        if team_a_matches.empty:
+            return 0
+        opposition_rank_diff = []
+        oppositions = []
+        for index, row in team_a_matches.iterrows():
+            if row['home_team'] == team:
+                oppositions.append([row['away_team'], row['date']])
+            else:
+                oppositions.append([row['home_team'], row['date']])
+        for opposition in oppositions:
+            team_rank, team_rank_change = self._get_closest_ranking_for_team(team, opposition[1])
+            opposition_rank, opposition_rank_change = self._get_closest_ranking_for_team(opposition[0], opposition[1])
+            opposition_rank_diff.append(opposition_rank - team_rank)
+        opposition_rank_diff = np.array(opposition_rank_diff)
+        return np.mean(opposition_rank_diff)
 
     def _get_head_to_head_statistics(self, home_team, away_team, date_of_new_match):
         self._match_results_df['date'] = pd.to_datetime(self._match_results_df['date'])
         matches_before_date = self._match_results_df[
             (self._match_results_df['date'] < pd.to_datetime(date_of_new_match)) &
-            ((self._match_results_df['home_team'] == home_team) & (
-                    self._match_results_df['away_team'] == away_team))]
+            (((self._match_results_df['home_team'] == home_team) & (self._match_results_df['away_team'] == away_team)) |
+             ((self._match_results_df['home_team'] == away_team) & (self._match_results_df['away_team'] == home_team)))
+            ]
         head_to_head_matches = matches_before_date.tail(self._head_to_head_window_size)
         if head_to_head_matches.empty:
             return 0, 0, 0, 0
-        mean_goals_home = head_to_head_matches['home_score'].mean()
-        mean_goals_away = head_to_head_matches['away_score'].mean()
-        std_goals_home = head_to_head_matches['home_score'].std()
-        std_goals_away = head_to_head_matches['away_score'].std()
-        return mean_goals_home, mean_goals_away, std_goals_home, std_goals_away
+        home_goals_scored = np.concatenate(
+            (head_to_head_matches[head_to_head_matches['home_team'] == home_team]['home_score'].to_numpy(),
+             head_to_head_matches[head_to_head_matches['away_team'] == home_team]['away_score'].to_numpy()))
+        away_goals_scored = np.concatenate(
+            (head_to_head_matches[head_to_head_matches['home_team'] == away_team]['home_score'].to_numpy(),
+             head_to_head_matches[head_to_head_matches['away_team'] == away_team]['away_score'].to_numpy()))
+        home_mean_goals_scored = np.mean(home_goals_scored)
+        away_mean_goals_scored = np.mean(away_goals_scored)
+        home_std_goals_scored = np.std(home_goals_scored)
+        away_std_goals_scored = np.std(away_goals_scored)
+        return home_mean_goals_scored, away_mean_goals_scored, home_std_goals_scored, away_std_goals_scored
 
     def _get_closest_ranking_for_team(self, team_name, target_date):
-        self._rankings_df['rank_date'] = pd.to_datetime(self._rankings_df['rank_date'])
         target_date = pd.to_datetime(target_date)
         team_df = self._rankings_df[
             (self._rankings_df['country_full'].str.lower() == team_name.lower()) & (
-                        self._rankings_df['rank_date'] <= target_date)]
+                    self._rankings_df['rank_date'] <= target_date)]
         if team_df.empty:
             return 0, 0
         closest_rank_date = team_df['rank_date'].max()
         closest_rank_row = team_df[team_df['rank_date'] == closest_rank_date].iloc[0]
         return closest_rank_row['rank'], closest_rank_row['rank_change']
 
-    def _get_home_win_count(self, team, date_of_new_match):
+    def _get_team_win_count(self, team, date_of_new_match):
         matches_before_date = self._match_results_df[
             (self._match_results_df['date'] < pd.to_datetime(date_of_new_match)) &
-            (self._match_results_df['home_team'] == team)
-            ]
-        home_wins = matches_before_date[matches_before_date['home_score'] > matches_before_date['away_score']].shape[0]
-        return home_wins
-
-    def _get_away_win_count(self, team, date_of_new_match):
-        matches_before_date = self._match_results_df[
-            (self._match_results_df['date'] < pd.to_datetime(date_of_new_match)) &
-            (self._match_results_df['away_team'] == team)
-            ]
-        away_wins = matches_before_date[matches_before_date['away_score'] > matches_before_date['home_score']].shape[0]
-        return away_wins
+            ((self._match_results_df['home_team'] == team) | (self._match_results_df['away_team'] == team))
+            ].tail(self._individual_window_size)
+        wins = matches_before_date[matches_before_date['home_score'] > matches_before_date['away_score']].shape[0]
+        return wins
 
     def _get_head_to_head_win_count(self, home_team, away_team, date_of_new_match):
         matches_before_date = self._match_results_df[
             (self._match_results_df['date'] < pd.to_datetime(date_of_new_match)) &
-            ((self._match_results_df['home_team'] == home_team) & (self._match_results_df['away_team'] == away_team))
-            ]
-        home_team_wins = matches_before_date[matches_before_date['home_score'] > matches_before_date['away_score']].shape[0]
-        away_team_wins = matches_before_date[matches_before_date['home_score'] < matches_before_date['away_score']].shape[0]
+            (((self._match_results_df['home_team'] == home_team) & (self._match_results_df['away_team'] == away_team)) |
+             ((self._match_results_df['home_team'] == away_team) & (self._match_results_df['away_team'] == home_team)))
+            ].tail(self._head_to_head_window_size)
+        home_team_wins = (matches_before_date[(matches_before_date['home_team'] == home_team) & (matches_before_date['home_score'] > matches_before_date['away_score'])].shape[0]) + (matches_before_date[(matches_before_date['away_team'] == home_team) & (matches_before_date['home_score'] < matches_before_date['away_score'])].shape[0])
+        away_team_wins = (matches_before_date[(matches_before_date['home_team'] == away_team) & (matches_before_date['home_score'] > matches_before_date['away_score'])].shape[0]) + (matches_before_date[(matches_before_date['away_team'] == away_team) & (matches_before_date['home_score'] < matches_before_date['away_score'])].shape[0])
         return home_team_wins, away_team_wins
+
+    def _get_match_type(self, match_type):
+        if match_type == 'Friendly':
+            return 1, 0, 0
+        elif 'qualification' in match_type.lower():
+            return 0, 1, 0
+        else:
+            return 0, 1, 1
