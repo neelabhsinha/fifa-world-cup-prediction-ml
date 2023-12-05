f9d86071f29772b7abdb36ca4c15bc4e1c89ac82
yuchenjohnlin <146393820+yuchenjohnlin@users.noreply.github.com>
2023-12-04 19:40:41 -0500

    kmeans.py betaCV & db_index metrics

--- model/kmeans.py
+++ model/kmeans.py
@@ -1,6 +1,8 @@
 from k_means_constrained import KMeansConstrained
 import numpy as np
 from sklearn.metrics import silhouette_score
+from sklearn.metrics import davies_bouldin_score
+from model.betaCV import betacv
 
 class Kmeans:
     def __init__(self, n_components=4, model_name= 'kmeans'):
@@ -23,10 +25,20 @@ class Kmeans:
             team_cluster_index = np.concatenate((team_cluster_index, random_team_index), axis=0)
 
         clusters = np.array(all_countries)[team_cluster_index]
-        print(f"Silhouette Score- {self.silhouette_average(features)}")
+        print(f"Silhouette Score - {self.silhouette_average(features)}")
+        print(f"Davies-Bouldin Index - {self.db_index_score(features)}")
+        print(f"Beta CV measure - {self.betaCV_score(features)}")
         return clusters.reshape(4,8)
 
     def silhouette_average(self, X):
         labels = self._model.fit_predict(X)
         silhouette_avg = silhouette_score(X, labels)
         return silhouette_avg
+        
+    def db_index_score(self, X):
+        db_index = davies_bouldin_score(X, self._model.labels_)
+        return db_index
+    
+    # from https://github.com/hayashikan/betacv/blob/master/betacv.py
+    def betaCV_score(self,X):
+        return betacv(X,self._model.labels_,metric='euclidean')
